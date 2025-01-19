d0021054ee266499c11762f24ad44c7331a2b331
abid-sami <ragib7264@gmail.com>
2025-01-19 21:06:29 +0600

    first commit

---
+++ First.js
@@ -0,0 +1,5 @@
+name = "Sami";
+address = "Gurudaspur, Natore";
+age = 22;
+console.log(name,address);
+console.log(age)
---
+++ product.js
@@ -0,0 +1,10 @@
+const account={
+    name: "Ragib Hasan Abid",
+    isFollow: false,
+    posts: 198,
+    followers: 595,
+    following: 4,
+    bio: "Nothing is Impossible",
+
+};
+console.log(account);
