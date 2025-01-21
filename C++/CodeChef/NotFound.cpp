2296cf7860249ff6e8805812e81de4d6233d50cc
abid-sami <ragib7264@gmail.com>
2025-01-21 23:16:10 +0600

    Submit

---
+++ C++/CodeChef/NotFound.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+    cin>>x;
+    if(x==404){
+        cout<<"Not Found"<<endl;
+    }
+    else{
+        cout<<"Found"<<endl;
+    }
+
+}
---
+++ C++/CodeChef/Score.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,n,a;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x>>n;
+        a=x/10;
+        cout<<a*n<<endl;
+    }
+
+}
