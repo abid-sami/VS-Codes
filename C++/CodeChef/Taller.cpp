fdaf5e0790ece25a3c14189a4c71c3ebb7361f83
abid-sami <ragib7264@gmail.com>
2025-01-22 02:15:30 +0600

    Submit

---
+++ C++/CodeChef/Taller.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,y;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x>>y;
+        if(x>y){
+            cout<<"A"<<endl;
+        }
+        else{
+            cout<<"B"<<endl;
+        }
+ 
+    }
+
+}
+
+
