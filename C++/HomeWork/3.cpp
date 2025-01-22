606a6c02e3c53ecc2fef86b9d70e33ee8bc0fc76
abid-sami <ragib7264@gmail.com>
2025-01-23 00:50:47 +0600

    Submi

--- C++/HomeWork/3.cpp
+++ C++/HomeWork/3.cpp
@@ -2,7 +2,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i;
+    int i, n[3];
     cout<<"Enter 3 Numbers: ";
-    
+    for(i=0; i<3; i++){
+        cin>>n[i];
+    }
+    int max=n[0];
+    for(i=0; i<3; i++){
+        if(max<n[i]){
+            max=n[i];
+        }
+    }
+    cout<<"Maximum Number: "<<max<<endl;
+
 } 
---
+++ C++/HomeWork/4.cpp
@@ -0,0 +1,1 @@
+//Write a C++ program that checks whether a given integer is even or odd. 
