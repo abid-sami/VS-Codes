b5cef0635d3cdce9bcd77fde3265361961a0bf94
abid-sami <ragib7264@gmail.com>
2025-01-26 01:22:08 +0600

    Submit

---
+++ C++/CodeChef/MessiRonaldo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b,x,y,c,d;
+	cin>>a>>b>>x>>y;
+    c=(a*2)+b;
+    d=(x*2)+y;
+	if(c>d){
+	    cout<<"Messi";
+	 
+	}
+	else if(c<d){
+	    cout<<"ronaldo";
+	}
+	else if(c=d){
+	    cout<<"Equal";
+	}
+
+}
---
+++ C++/CodeChef/WaitingTime.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, k, x, i, a;
+	cin>>t;
+    for(i=0; i<t; i++){
+        cin>>k>>x;
+        a=(k*7)-x;
+        cout<<a<<endl;
+    }
+    
+
+}
