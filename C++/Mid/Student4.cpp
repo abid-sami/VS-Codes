564bd5a30b9e85b11da5ff37adc99eb35f25bf9f
abid-sami <ragib7264@gmail.com>
2025-01-29 02:32:38 +0600

    Push

---
+++ C++/Mid/Student4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+    int id;
+    string name;
+    float cgpa;
+    public:
+    void set_value(){
+        cin>>id>>name>>cgpa;
+    }
+    void set_display(){
+        cout<<"ID: "<<id<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"CGPA: "<<cgpa<<endl;
+    }
+};
+int main(){
+    student st1;
+    st1.set_value();
+    st1.set_display();
+}
