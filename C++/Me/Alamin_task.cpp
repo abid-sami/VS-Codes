8c02f2b796298e0ace502bb49706408ff2aaa0ff
abid-sami <ragib7264@gmail.com>
2025-04-16 17:32:26 +0600

    Push

---
+++ C++/Me/Alamin_task.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+    int id;
+    int roll;
+    string name;
+    string dept;
+    string designation;
+    float gpa;
+};
+
+class Student{
+    
+};
