ac570cab4f1d9302604f98e0a9556112de65fff4
abid-sami <ragib7264@gmail.com>
2025-04-17 20:18:02 +0600

    Push

--- C++/Me/Alamin_task.cpp
+++ C++/Me/Alamin_task.cpp
@@ -8,8 +8,87 @@ class Person{
     string dept;
     string designation;
     float gpa;
+
+public:
+    void input(int ID, int ROLL, string NAME, string DEPT, string DESIGNATION, float GPA){
+        id=ID, roll=ROLL, name=NAME, dept=DEPT, designation=DESIGNATION, gpa=GPA;
+    }
+
+    void student_input(){
+        cin.ignore();
+        cout<<"Enter Student Name: "<<endl;
+        getline(cin,name);
+        cout<<"Enter Roll: "<<endl;
+        cin>>roll;
+        cin.ignore();
+        cout<<"Enter Dept: "<<endl;
+        getline(cin, dept);
+        cout<<"Enter GPA: "<<endl;
+        cin>>gpa;
+    }
+
+    void display_student(){
+        cout<<"Student Name: "<<name<<endl;
+        cout<<"Roll: "<<roll<<endl;
+        cout<<"Dept: "<<dept<<endl;
+        cout<<"GPA: "<<gpa<<endl;
+        
+    }
+
+    void teacher_input(){
+        cin.ignore();
+        cout<<"Enter Teacher Name: "<<endl;
+        getline(cin,name);
+        cout<<"Enter ID: "<<endl;
+        cin>>id;
+        cin.ignore();
+        cout<<"Enter Designation: "<<endl;
+        getline(cin,designation);
+
+    }
+
+    void display_teacher(){
+        cout<<"Teacher Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+        cout<<"Designation: "<<designation<<endl;
+        
+    }
 };
 
-class Student{
+class Student:public Person{
+    public:
+    void student_call(){
+        student_input();
+        display_student();
+    }
     
 };
+
+class Teacher:public Person{
+    public:
+    void teacher_call(){
+        teacher_input();
+        display_teacher();
+    }
+
+    
+};
+
+int main(){
+    int t;
+    cout<<"Enter Amount: "<<endl;
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+    
+        Student s1;
+        s1.student_call();
+        //s1.display_student();
+        
+        Teacher t1;
+        t1.teacher_call();
+        //t1.display_teacher();
+    
+    }
+ 
+}
--- C++/Me/tempCodeRunnerFile.cpp
+++ C++/Me/tempCodeRunnerFile.cpp
@@ -1,19 +1,94 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main(){
-    string a, b;
-    cin>>a>>b;
-    int n=a.length();
-    for(int i=0; i<n; i++){
-        if(a[i]!=b[n-1-i]){
-            cout<<"NO"<<endl;
-            break;
-        }
-        else{
-            cout<<"YES"<<endl;
-            break;
-        }
+class Person{
+    int id;
+    int roll;
+    string name;
+    string dept;
+    string designation;
+    float gpa;
+
+public:
+    void input(int ID, int ROLL, string NAME, string DEPT, string DESIGNATION, float GPA){
+        id=ID, roll=ROLL, name=NAME, dept=DEPT, designation=DESIGNATION, gpa=GPA;
+    }
+
+    void student_input(){
+        cin.ignore();
+        cout<<"Enter Student Name: "<<endl;
+        getline(cin,name);
+        cout<<"Enter Roll: "<<endl;
+        cin>>roll;
+        cin.ignore();
+        cout<<"Enter Dept: "<<endl;
+        getline(cin, dept);
+        cout<<"Enter GPA: "<<endl;
+        cin>>gpa;
+    }
+
+    void display_student(){
+        cout<<"Student Name: "<<name<<endl;
+        cout<<"Roll: "<<roll<<endl;
+        cout<<"Dept: "<<dept<<endl;
+        cout<<"GPA: "<<gpa<<endl;
+        
+    }
+
+    void teacher_input(){
+        cin.ignore();
+        cout<<"Enter Teacher Name: "<<endl;
+        getline(cin,name);
+        cout<<"Enter ID: "<<endl;
+        cin>>id;
+        cin.ignore();
+        cout<<"Enter Designation: "<<endl;
+        getline(cin,designation);
+
+    }
+
+    void display_teacher(){
+        cout<<"Teacher Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+        cout<<"Designation: "<<designation<<endl;
+        
+    }
+};
+
+class Student:public Person{
+    public:
+    void student_call(){
+        student_input();
+        display_student();
     }
     
+};
+
+class Teacher:public Person{
+    public:
+    void teacher_call(){
+        teacher_input();
+        display_teacher();
+    }
+
+    
+};
+
+int main(){
+    int t;
+    cout<<"Enter Amount: "<<endl;
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+    
+        Student s1;
+        s1.student_call();
+        //s1.display_student();
+        
+        Teacher t1;
+        t1.teacher_call();
+        //t1.display_teacher();
+    
+    }
+ 
 }
