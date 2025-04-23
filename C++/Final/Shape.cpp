9fd3be08a89790ddefd413b2592e1bc1b39e8ae0
abid-sami <ragib7264@gmail.com>
2025-04-23 20:10:45 +0600

    Push

--- C++/Final/Shape.cpp
+++ C++/Final/Shape.cpp
@@ -1,52 +1,111 @@
-#include<iostream>
+//Always try to do it in Private & it can be questioned as "Just use Constructor, No Function"
+
+#include <iostream>
 #define PI 3.1416
 using namespace std;
 
-class Shape{
-    public:
+class Shape
+{
     float area, volume;
+    public:
+    void set_value(float a, float v)
+    {
+        area=a, volume=v;
+    }
     void display(){
-        cout<<"Area: "<<area<<endl;
-        cout<<"Volume: "<<volume<<endl;
+        cout << "Area: " << area << endl;
+        cout << "Volume: " << volume << endl;
     }
 };
 
-class Rectangle:public Shape{
-    float length, width;
-    public:
-    Rectangle(float l, float w){
-        length=l;
-        width=w;
-    }
-    void calculate(){
-        area=length*width;
-    }
-    void display();
-};
+class Rectangle : public Shape
+{
+    float height, width, length;
 
-class Circle:public Shape{
-    float radius;
-    public:
-    Circle(float r){
-        radius=r;
+public:
+    Rectangle(float h, float w, float l)
+    {
+        length = l, width = w, height = h;
     }
-    void calculate(){
-        area=PI*radius*radius;
+    void calculate()
+    {
+        float area = height * width;
+        float volume = height * width * length;
+        set_value(area, volume); //IMPORTANT
+        display(); //IMPORTANT
     }
 };
 
-class Triangle:public Shape{
-    float base, height;
-    public:
-    Triangle(float b, float h){
-        base=b;
+class Circle : public Shape
+{
+    float radius, height;
+
+public:
+    Circle(float r, float h)
+    {
+        radius = r;
         height=h;
     }
-    void calculate(){
-        area=0.5*height*base;
+    void calculate()
+    {
+        float area = PI * radius * radius;
+        float volume = PI * radius * height;
+        set_value(area, volume);
+        display();
     }
 };
 
-int main(){
-    Rectangle r(4,5);
+class Square : public Shape
+{
+    float length;
+
+public:
+    Square(float l)
+    {
+        length = l;
+    }
+    void calculate()
+    {
+        float area = length * length;
+        float volume = length * length* length;
+        set_value(area, volume);
+        display();
+    }
+};
+
+class Triangle : public Shape
+{
+    float base, height, length;
+
+public:
+    Triangle(float b, float h, float l)
+    {
+        base = b, height = h, length = l;
+    }
+    void calculate()
+    {
+        float area = 0.5 * base * height;
+        float volume = 0.5 * base * height * length;
+        set_value(area, volume);
+        display();
+    }
+};
+
+int main()
+{
+    Rectangle r(4, 5, 6);
+    cout<<"...Rectangle..."<<endl;
+    r.calculate();
+
+    Circle c(4, 5);
+    cout<<"...Circle..."<<endl;
+    c.calculate();
+
+    Square s(5);
+    cout<<"...Squre..."<<endl;
+    s.calculate();
+
+    Triangle t(4, 5, 6);
+    cout<<"...Triangle..."<<endl;
+    t.calculate();
 }
