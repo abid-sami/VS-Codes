984d3e2b4b375be8ce33cda3970d1c8b4b8f3fe1
abid-sami <ragib7264@gmail.com>
2025-05-12 13:06:44 +0600

    Push

---
+++ C++/Final/ExceptionHandling.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b, rem;
+    cin>>a>>b;
+    try{
+        if(b!=0){
+            rem=a%b;
+        }
+        else{
+            throw 0;
+        }
+        cout<<"Remainder "<<rem<<endl;
+    }
+    catch(int e){
+        cout<<"Can't be devided by "<<e<<endl;
+    }
+    cout<<"Hello World"<<endl;
+}
