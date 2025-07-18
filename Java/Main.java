9eab159a793f7003f0839c25b9112331092ccb5f
abid-sami <ragib7264@gmail.com>
2025-07-18 18:48:50 +0600

    Push

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,26 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, Sami");
+        //Output
+        String name="Sami";
+        System.out.println(name.charAt(1));
+
+        //Length of the string
+        System.out.println(name.length());
+
+        //CharAt
+        System.out.println(name.charAt(0));
+
+        //Replace
+        System.out.println(name.replace('S', 's'));
+
+        //Substring
+        System.out.println(name.substring(1, 3));
+
+        //Array
+        int [] marks = {10, 20, 30};
+        System.out.println(marks[0]);
     }
     
 }
+ 
